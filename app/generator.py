417dbac016f1f0f6f878d402e3987b464cb0867e
Martin Kromkamp <martink@schoolmaster.nl>
2017-02-03 15:29:14 +0100

    Generate graph

--- app/generator.py
+++ app/generator.py
@@ -1,16 +1,48 @@
 import argparse
 import json
 
+import pygraphviz as pgv
 from broker import Broker
 
+def parse_args():
+    ''' Get cli args '''
+    parser = argparse.ArgumentParser(description='RabbitMQ topology generator')
+    parser.add_argument('-i', '--infile',
+                        type=argparse.FileType('r'),
+                        required=True,
+                        help='Broker definition file')
+    parser.add_argument('-o', '--outfile',
+                        type=argparse.FileType('w'),
+                        required=True,
+                        help='Output file')
 
-parser = argparse.ArgumentParser(description='RabbitMQ topology generator')
-parser.add_argument('-i', '--infile', type=argparse.FileType('r'), required=True, help='Broker definition file')
-parser.add_argument('-o', '--outfile', type=argparse.FileType('w'), required=True, help='Output .dot file')
-args = parser.parse_args()
+    return parser.parse_args()
 
-broker = Broker(json.loads(args.infile.read()))
+def gen():
+    ''' Generator '''
+    args = parse_args()
+    broker = Broker(json.loads(args.infile.read()))
 
+    # Create graph
+    graph = pgv.AGraph(directed=True, strict=False)
 
-for e in broker.exchange_names():
-    print(e)
+    for exchange in broker.exchanges():
+        graph.add_node(exchange.name)
+
+    for queue in broker.queues():
+        graph.add_node(queue.name)
+
+    for binding in broker.bindings():
+        graph.add_edge(binding.source, binding.destination, label=binding.routing_key)
+        # print('{} --- {} --- {}'.format(binding.source, binding.routing_key, binding.destination))
+
+    for policy in broker.policies():
+        if policy.pattern is not None and policy.definition.dead_letter_exchange is not None:
+            graph.add_edge(policy.pattern, policy.definition.dead_letter_exchange,
+                           label=policy.definition.dead_letter_routing_key)
+
+    # graph.layout(prog='circo')
+    # graph.write(args.outfile)
+    graph.draw(args.outfile.name, prog='circo')
+
+gen()
