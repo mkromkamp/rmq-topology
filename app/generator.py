e29f502655d959ee9b98f6a9bf1ad08c4d9adebe
Martin Kromkamp <mkromkamp@gmail.com>
2017-02-06 14:47:09 +0100

    updated font to arial

--- app/generator.py
+++ app/generator.py
@@ -29,20 +29,22 @@ def main():
 
     for exchange in broker.exchanges():
         graph.add_node(exchange.name, label=exchange.label(), style='filled',
-                       shape='rectangle', fillcolor='#3333CC', fontcolor='#ffffff')
+                       shape='rectangle', fillcolor='#3333CC', fontcolor='#ffffff', 
+                       fontname='arial')
 
     for queue in broker.queues():
         graph.add_node(queue.name, label=queue.label(),
-                       style='filled', shape='rectangle', fillcolor='#FF0000')
+                       style='filled', shape='rectangle', fillcolor='#FF0000', fontname='arial')
 
     for binding in broker.bindings():
         graph.add_edge(binding.source, binding.destination, label=binding.routing_key,
-                       group=binding.source)
+                       group=binding.source, fontname='arial')
 
     for policy in broker.policies():
         if policy.pattern is not None and policy.definition.dead_letter_exchange is not None:
             graph.add_edge(policy.pattern, policy.definition.dead_letter_exchange,
-                           label=policy.definition.dead_letter_routing_key, group=policy.pattern)
+                           label=policy.definition.dead_letter_routing_key, group=policy.pattern,
+                           fontname='arial')
 
     graph.layout(prog='dot')
     graph.write(args.outfile)
